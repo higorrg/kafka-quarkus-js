ec6e4397760dfcd6e1740584965865d1ba15ebd7
Higor Granzoto <higor.granzoto@shift.com.br>
2021-07-01 10:10:55 -0300

    Fix runner scripts

--- kafka-quarkus-consumer/celsius.sh
+++ kafka-quarkus-consumer/celsius.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ ! -f kafka-consumer-1.0-SNAPSHOT-runner.jar ]; then
+if [ ! -f target/kafka-quarkus-consumer-1.0-SNAPSHOT-runner.jar ]; then
   mvn clean package
 fi
-java -Dkelvin-diff=-272.15 -Dtemperature-name=Celsius -Dquarkus.http.port=8082 -jar target/kafka-consumer-1.0-SNAPSHOT-runner.jar
+java -Dkelvin-diff=-272.15 -Dtemperature-name=Celsius -Dquarkus.http.port=8082 -jar target/kafka-quarkus-consumer-1.0-SNAPSHOT-runner.jar
--- kafka-quarkus-consumer/fahrenheit.sh
+++ kafka-quarkus-consumer/fahrenheit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ ! -f kafka-consumer-1.0-SNAPSHOT-runner.jar ]; then
+if [ ! -f target/kafka-quarkus-consumer-1.0-SNAPSHOT-runner.jar ]; then
   mvn clean package
 fi
-java -Dkelvin-diff=-457.87 -Dtemperature-name=Fahrenheit -Dquarkus.http.port=8083 -jar target/kafka-consumer-1.0-SNAPSHOT-runner.jar
+java -Dkelvin-diff=-457.87 -Dtemperature-name=Fahrenheit -Dquarkus.http.port=8083 -jar target/kafka-quarkus-consumer-1.0-SNAPSHOT-runner.jar
