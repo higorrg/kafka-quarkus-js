26e1281883cd89fe817a0ba23d23ebc58f36ca13
Higor Granzoto <higor.granzoto@shift.com.br>
2021-07-01 20:57:18 -0300

    Improve kelvin converter

--- kafka-quarkus-consumer/src/main/java/org/acme/kafka/KelvinConverter.java
+++ kafka-quarkus-consumer/src/main/java/org/acme/kafka/KelvinConverter.java
@@ -7,8 +7,6 @@ import org.eclipse.microprofile.reactive.messaging.Acknowledgment;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.eclipse.microprofile.reactive.messaging.Outgoing;
 
-import io.smallrye.reactive.messaging.annotations.Broadcast;
-
 @ApplicationScoped
 public class KelvinConverter {
 
@@ -17,8 +15,7 @@ public class KelvinConverter {
 
     @Incoming("temperature")
     @Outgoing("temperature-stream")
-    @Broadcast
-    @Acknowledgment(Acknowledgment.Strategy.PRE_PROCESSING)
+	@Acknowledgment(Acknowledgment.Strategy.POST_PROCESSING)
     public double process(int temperatureInKelvin) {
         return temperatureInKelvin + kelvinDiff;
     }
