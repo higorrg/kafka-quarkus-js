c26e1ffa8bdc23130305ff2143084241f2d9fc3e
Higor Granzoto <higor.granzoto@shift.com.br>
2021-07-01 20:14:37 -0300

    Rollback from quarkus 2.0.0.Final to 1.9.0.Final

--- kafka-quarkus-consumer/celsius.sh
+++ kafka-quarkus-consumer/celsius.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-if [ ! -f target/quarkus-app/quarkus-run.jar ]; then
+if [ ! -f target/kafka-quarkus-consumer-1.0-SNAPSHOT-runner.jar ]; then
   echo "Clean Package"
   mvn clean package
 fi
-java -Dkelvin-diff=-272.15 -Dtemperature-name=Celsius -Dquarkus.http.port=8082 -jar target/quarkus-app/quarkus-run.jar
+java -Dkelvin-diff=-272.15 -Dtemperature-name=Celsius -Dquarkus.http.port=8082 -jar target/kafka-quarkus-consumer-1.0-SNAPSHOT-runner.jar
--- kafka-quarkus-consumer/fahrenheit.sh
+++ kafka-quarkus-consumer/fahrenheit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-if [ ! -f target/quarkus-app/quarkus-run.jar ]; then
+if [ ! -f target/kafka-quarkus-consumer-1.0-SNAPSHOT-runner.jar ]; then
   echo "Clean Package"
   mvn clean package
 fi
-java -Dkelvin-diff=-457.87 -Dtemperature-name=Fahrenheit -Dquarkus.http.port=8083 -jar target/quarkus-app/quarkus-run.jar
+java -Dkelvin-diff=-457.87 -Dtemperature-name=Fahrenheit -Dquarkus.http.port=8083 -jar target/kafka-quarkus-consumer-1.0-SNAPSHOT-runner.jar
