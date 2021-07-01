63dc7ffc276f3d71363bf12cbb7102a7e458020c
Higor Granzoto <higor.granzoto@shift.com.br>
2021-07-01 20:56:29 -0300

    Inprove consumer scripts log

--- kafka-quarkus-consumer/celsius.sh
+++ kafka-quarkus-consumer/celsius.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ ! -f target/kafka-quarkus-consumer-1.0-SNAPSHOT-runner.jar ]; then
-  echo "Clean Package"
+  echo "Jar not found: Clean Package"
   mvn clean package
 fi
 java -Dkelvin-diff=-272.15 -Dtemperature-name=Celsius -Dquarkus.http.port=8082 -jar target/kafka-quarkus-consumer-1.0-SNAPSHOT-runner.jar
--- kafka-quarkus-consumer/fahrenheit.sh
+++ kafka-quarkus-consumer/fahrenheit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ ! -f target/kafka-quarkus-consumer-1.0-SNAPSHOT-runner.jar ]; then
-  echo "Clean Package"
+  echo "Jar not found: Clean Package"
   mvn clean package
 fi
 java -Dkelvin-diff=-457.87 -Dtemperature-name=Fahrenheit -Dquarkus.http.port=8083 -jar target/kafka-quarkus-consumer-1.0-SNAPSHOT-runner.jar
